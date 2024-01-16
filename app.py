2ff753a5e93ea9ab299c152457b5dbab7056f094
Eylül Özatman <eylulozatman@gmail.com>
2024-01-16 03:59:10 +0300

    commit neww

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from models import db, Request
-from queue import Queue
+from models import db, Request, FoundMessage
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from flasgger import Swagger
+import logging
 
 app = Flask(__name__)
+Swagger(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///requestService.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-my_queue = Queue() 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def homepage():
+    return render_template('homepage.html')
+
+@app.route('/createRequest', methods=['GET', 'POST'])
+def createRequest():
     if request.method == 'POST':
         blood_type = request.form['bloodtype']
         town = request.form['town']
@@ -49,6 +54,7 @@ def get_queue_data():
     result = []
     for request in request_list:
         data = {
+            'id':request.id,
             'blood_type': request.blood_type,
             'town': request.town,
             'city': request.city,
@@ -58,7 +64,41 @@ def get_queue_data():
     
     return jsonify(result)
 
+@app.route('/bloodFound', methods=['POST','GET'])
+def bloodFound():
+    if request.method == 'POST':
+        data = request.get_json()
+        app.logger.info(data)
+
+        request_id = data.get('id')
+        message = data.get('message')
+        available_units = data.get('available_units')
+
+        new_message = FoundMessage(request_id=request_id, message=message)
+        db.session.add(new_message)
+        db.session.commit()
+        
+        response_data = {
+            'status': 'Success',
+            'message': f'Received message for request id {request_id}: {message}'
+        }
+
+        app.logger.info(message)
+
+        request_to_delete = Request.query.filter_by(id=request_id).first()
+        if request_to_delete:
+            if available_units < request_to_delete.num_of_units:
+                # Num_of_units - available_units ile request'in kalan ihtiyacını güncelle
+                request_to_delete.num_of_units -= available_units
+                db.session.commit()
+            else:
+                db.session.delete(request_to_delete)
+                db.session.commit()
+
+    messages = FoundMessage.query.all()
+    return render_template('messages.html', messages=messages)
+
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     app.run(port=8686)
-
--- models.py
+++ models.py
@@ -11,3 +11,9 @@ class Request(db.Model):
     num_of_units = db.Column(db.Integer, nullable=False)
     duration_of_search = db.Column(db.String(50), nullable=False)
     reason = db.Column(db.String(100), nullable=False)
+
+class FoundMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    request_id = db.Column(db.Integer, db.ForeignKey('request.id'), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
+    request = db.relationship('Request', backref='found_messages')
